a8cedacf517e3bffbbe68eda54756cd1f841ab46
huizhubo <xahdp@foxmail.com>
2018-06-14 17:17:18 +0800

    09-配置config和sqlalchemy（掌握）

--- manager.py
+++ manager.py
@@ -1,5 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# 配置信息
+class Config(object):
+    DEBUG = True
+
+    # 配置mysql数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/information11'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -8,4 +24,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
