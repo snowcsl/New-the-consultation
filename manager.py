0644ee6cb93c5df3fd85916757a06d6b599d3877
huizhubo <xahdp@foxmail.com>
2018-06-15 16:50:37 +0800

    忽略了logswenjianjia

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 import logging
+from flask import current_app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db
@@ -18,11 +19,17 @@ def hello_world():
     # redis_store.setex('name', 10, 'itheima')
     # session['name'] = 'zhubo'
 
-    logging.fatal('fatal')
-    logging.error('error')
-    logging.warning('warning')
-    logging.info('info')
-    logging.debug('debug')
+    # logging.fatal('fatal')
+    # logging.error('error')
+    # logging.warning('warning')
+    # logging.info('info')
+    # logging.debug('debug')
+
+    current_app.logger.fatal('fatal')
+    current_app.logger.error('error')
+    current_app.logger.warning('warning')
+    current_app.logger.info('info')
+    current_app.logger.debug('debug')
 
     return 'Hello World!'
 
