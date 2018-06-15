38c60ccae52de6a19bf20b034cd4c12759572561
huizhubo <xahdp@foxmail.com>
2018-06-15 17:40:48 +0800

    13-加载首页路由地址&favicon图标加载（掌握）

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,23 +1,18 @@
 # 2. 导入创建的蓝图对象, 使用蓝图实现路由
 from . import index_blue
 from info import redis_store
+from flask import render_template, current_app
 
 
 @index_blue.route('/')
-def hello_world():
-    redis_store.setex('name', 10, 'itheima')
-    # session['name'] = 'zhubo'
+def index():
+    return render_template('news/index.html')
 
-    # logging.fatal('fatal')
-    # logging.error('error')
-    # logging.warning('warning')
-    # logging.info('info')
-    # logging.debug('debug')
 
-    # current_app.logger.fatal('fatal')
-    # current_app.logger.error('error')
-    # current_app.logger.warning('warning')
-    # current_app.logger.info('info')
-    # current_app.logger.debug('debug')
-
-    return 'Hello World!'
+# 浏览器会自动请求该地址, 以获取网站的图标
+@index_blue.route('/favicon.ico')
+def favicon():
+    # current_app: 是一次请求中, 产生的app的对象. 能够拥有所有app的属性和方法
+    # 使用时无需关心原来的app在那个文件中
+    # send_static_file: 发送静态文件. 查找到目录默认从static开始的
+    return current_app.send_static_file('news/favicon.ico')
