32381e254553c0997c2cd6682e32ba0e5007a400
huizhubo <xahdp@foxmail.com>
2018-06-21 17:44:18 +0800

    12-首页点击排行数据查询（掌握）

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,8 @@
 # 2. 导入创建的蓝图对象, 使用蓝图实现路由
-from info.models import User, Category
+from info.models import User, Category, News
 from info.utils.response_code import RET
 from . import index_blue
-from info import redis_store
+from info import redis_store, constants
 from flask import render_template, current_app, session, jsonify
 
 
@@ -37,12 +37,25 @@ def index():
     for category in category_models if category_models else []:
         category_list.append(category.to_dict())
 
+    # 三. 点击排行信息
+    try:
+        # limit: 会按照设置的数量来返回结果
+        news_models = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库错误")
+
+    click_news_list = []
+    for news in news_models if news_models else []:
+        click_news_list.append(news.to_basic_dict())
+
     # 封装成data字典, 传入模板
     data = {
         # 在处理不同接口的返回数据时, 不需要全部返回, 可以值返回需要的数据
         # user.to_index_dict(): 将模型对象转换为需要的数据字典
         'user': user.to_index_dict() if user else None,
-        'category_list': category_list
+        'category_list': category_list,
+        'click_news_list': click_news_list
     }
 
     return render_template('news/index.html', data=data)
