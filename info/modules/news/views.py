470331c54d39b5258ce033fb4e2b6fbcb964fa8a
huizhubo <xahdp@foxmail.com>
2018-06-22 16:58:36 +0800

    13-收藏的后端代码实现（掌握）

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -6,6 +6,60 @@ from info import redis_store, constants, db
 from flask import render_template, current_app, session, jsonify, request, g
 
 
+# 收藏和取消收藏
+@news_blue.route('/news_collect', methods=['POST'])
+@user_login_data
+def news_collect():
+
+    # 一. 获取参数
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    news_id = request.json.get('news_id')
+    action = request.json.get('action')
+
+    # 二. 校验参数
+    if not all([news_id, action]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ['collect', 'cancel_collect']:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 三. 逻辑处理
+    # 1. 查询新闻
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="无数据")
+
+    # 2. 根据按钮点击的值来进行添加或删除
+    if action == 'collect':
+        # 先判断用户没有收藏过,才能收藏
+        if news not in user.collection_news:
+            user.collection_news.append(news)
+    else:
+        # 先判断用户收藏过, 才能删除
+        if news in user.collection_news:
+            user.collection_news.remove(news)
+
+    # 3. 提交数据
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据库错误")
+
+    # 四. 返回数据
+    return jsonify(errno=RET.OK, errmsg="成功")
+
+
+# 获取新闻详情
 @news_blue.route('/<news_id>')
 @user_login_data
 def get_news_detail(news_id):
