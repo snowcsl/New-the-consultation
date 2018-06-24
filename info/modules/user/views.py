a229a805265dcded9c30eb545dcad5fec9055fc1
huizhubo <xahdp@foxmail.com>
2018-06-24 16:19:14 +0800

    07-加载用户信息页（掌握）

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -6,10 +6,21 @@ from info import redis_store, constants, db
 from flask import render_template, current_app, session, jsonify, request, g, redirect
 
 
+@user_blue.route('/base_info')
+@user_login_data
+def base_info():
+    user = g.user
+    data = {
+        'user': user.to_dict()
+    }
+    return render_template('news/user_base_info.html', data=data)
+
+
 @user_blue.route('/info')
 @user_login_data
 def info():
     user = g.user
+    # 目前我们只需要在这里判断一次用户没登录. 因为如果咱们大的模板页面都无法加载, 小页面也是无法加载的
     if not user:
         return redirect('/')
     data = {
